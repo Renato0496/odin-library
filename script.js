51ad0982b8cb9e2bf6b53c2f4384f8243fcb8841
Renato0496 <riar96@gmail.com>
2021-10-18 16:02:35 -0500

    First commit 2.0

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let myLibrary = [];
-let newBook;
-
-function Book(title, author, pages, read) {
+function Book(title,author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.info = function() {
+        if (this.read === 'yes') {
+            message = 'read.';
+        } else {
+            message = 'not read yet.';
+        }
+        return ''+title+' by '+author+', '+pages+' pages, '+message;
+    };
 }
-
-function addBookToLibrary() {
-    
-  }
