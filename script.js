7896dc1c7049bd85e703fa6c20e850c0d73bd87f
Renato0496 <riar96@gmail.com>
2021-10-21 16:04:44 -0500

    Added the modals button script

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-function Book(title,author, pages, read){
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    this.info = function () {
         if (this.read === 'yes') {
             message = 'read.';
         } else {
             message = 'not read yet.';
         }
-        return ''+title+' by '+author+', '+pages+' pages, '+message;
+        return '' + title + ' by ' + author + ', ' + pages + ' pages, ' + message;
     };
+};
+
+function addBookToLibrary(){
+    
 }
+
+// Modal
+
+let modal = document.getElementById('myFirstModal');
+let openModBut = document.getElementById('openModal');
+let closeMod = document.getElementsByClassName('close')[0];
+
+openModBut.onclick = () => {
+    modal.style.display = 'block';
+};
+
+closeMod.onclick = () => {
+    modal.style.display = 'none';
+};
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
